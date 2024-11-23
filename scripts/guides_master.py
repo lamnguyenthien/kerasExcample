92a3e11f19e9b1f0b944a6d37bbd869fa27635e9
lamnguyenthien <thienlamntl22@gmail.com>
2024-11-23 10:04:37 +0700

    Initial commit

---
+++ scripts/guides_master.py
@@ -0,0 +1,243 @@
+CV_GUIDES_MASTER = {
+    "path": "keras_cv/",
+    "title": "KerasCV",
+    "toc": True,
+    "children": [
+        {
+            "path": "object_detection_keras_cv",
+            "title": "Use KerasCV to assemble object detection pipelines",
+        },
+        {
+            "path": "classification_with_keras_cv",
+            "title": "Use KerasCV to train powerful image classifiers.",
+        },
+        {
+            "path": "cut_mix_mix_up_and_rand_augment",
+            "title": "CutMix, MixUp, and RandAugment image augmentation with KerasCV",
+        },
+        {
+            "path": "generate_images_with_stable_diffusion",
+            "title": "High-performance image generation using Stable Diffusion in KerasCV",
+        },
+        {
+            "path": "custom_image_augmentations",
+            "title": "Custom Image Augmentations with BaseImageAugmentationLayer",
+        },
+        {
+            "path": "semantic_segmentation_deeplab_v3_plus",
+            "title": "Semantic Segmentation with KerasCV",
+        },
+        {
+            "path": "segment_anything_in_keras_cv",
+            "title": "Segment Anything in KerasCV",
+        },
+    ],
+}
+
+NLP_GUIDES_MASTER = {
+    "path": "keras_nlp/",
+    "title": "KerasNLP",
+    "toc": True,
+    "children": [
+        {
+            "path": "getting_started",
+            "title": "Getting Started with KerasNLP",
+        },
+        {
+            "path": "transformer_pretraining",
+            "title": "Pretraining a Transformer from scratch with KerasNLP",
+        },
+        {
+            "path": "upload",
+            "title": "Uploading Models with KerasNLP",
+        },
+    ],
+}
+
+HUB_GUIDES_MASTER = {
+    "path": "keras_hub/",
+    "title": "KerasHub",
+    "toc": True,
+    "children": [
+        {
+            "path": "getting_started",
+            "title": "Getting Started with KerasHub",
+        },
+        {
+            "path": "semantic_segmentation_deeplab_v3",
+            "title": "Semantic Segmentation with KerasHub",
+        },
+        {
+            "path": "transformer_pretraining",
+            "title": "Pretraining a Transformer from scratch with KerasHub",
+        },
+        {
+            "path": "upload",
+            "title": "Uploading Models with KerasHub",
+        },
+        {
+            "path": "classification_with_keras_hub",
+            "title": "Classification with KerasHub",
+        },
+        {
+            "path": "segment_anything_in_keras_hub",
+            "title": "Segment Anything in KerasHub",
+        },
+        {
+            "path": "stable_diffusion_3_in_keras_hub",
+            "title": "Stable Diffusion 3 in KerasHub",
+        },
+    ],
+}
+
+KT_GUIDES_MASTER = {
+    "path": "keras_tuner/",
+    "title": "Hyperparameter Tuning",
+    "toc": True,
+    "children": [
+        {
+            "path": "getting_started",
+            "title": "Getting started with KerasTuner",
+        },
+        {
+            "path": "distributed_tuning",
+            "title": "Distributed hyperparameter tuning with KerasTuner",
+        },
+        {
+            "path": "custom_tuner",
+            "title": "Tune hyperparameters in your custom training loop",
+        },
+        {
+            "path": "visualize_tuning",
+            "title": "Visualize the hyperparameter tuning process",
+        },
+        {
+            "path": "failed_trials",
+            "title": "Handling failed trials in KerasTuner",
+        },
+        {
+            "path": "tailor_the_search_space",
+            "title": "Tailor the search space",
+        },
+    ],
+}
+
+GUIDES_MASTER = {
+    "path": "guides/",
+    "title": "Developer guides",
+    "toc": True,
+    "children": [
+        {
+            "path": "functional_api",
+            "title": "The Functional API",
+        },
+        {
+            "path": "sequential_model",
+            "title": "The Sequential model",
+        },
+        {
+            "path": "making_new_layers_and_models_via_subclassing",
+            "title": "Making new layers & models via subclassing",
+        },
+        {
+            "path": "training_with_built_in_methods",
+            "title": "Training & evaluation with the built-in methods",
+        },
+        {
+            "path": "custom_train_step_in_jax",
+            "title": "Customizing `fit()` with JAX",
+        },
+        {
+            "path": "custom_train_step_in_tensorflow",
+            "title": "Customizing `fit()` with TensorFlow",
+        },
+        {
+            "path": "custom_train_step_in_torch",
+            "title": "Customizing `fit()` with PyTorch",
+        },
+        {
+            "path": "writing_a_custom_training_loop_in_jax",
+            "title": "Writing a custom training loop in JAX",
+        },
+        {
+            "path": "writing_a_custom_training_loop_in_tensorflow",
+            "title": "Writing a custom training loop in TensorFlow",
+        },
+        {
+            "path": "writing_a_custom_training_loop_in_torch",
+            "title": "Writing a custom training loop in PyTorch",
+        },
+        {
+            "path": "serialization_and_saving",
+            "title": "Serialization & saving",
+        },
+        {
+            "path": "customizing_saving_and_serialization",
+            "title": "Customizing saving & serialization",
+        },
+        {
+            "path": "writing_your_own_callbacks",
+            "title": "Writing your own callbacks",
+        },
+        {
+            "path": "transfer_learning",
+            "title": "Transfer learning & fine-tuning",
+        },
+        {
+            "path": "distributed_training_with_jax",
+            "title": "Distributed training with JAX",
+        },
+        {
+            "path": "distributed_training_with_tensorflow",
+            "title": "Distributed training with TensorFlow",
+        },
+        {
+            "path": "distributed_training_with_torch",
+            "title": "Distributed training with PyTorch",
+        },
+        {
+            "path": "distribution",
+            "title": "Distributed training with Keras 3",
+        },
+        {
+            "path": "migrating_to_keras_3",
+            "title": "Migrating Keras 2 code to Keras 3",
+        },
+        # {
+        #     "path": "preprocessing_layers",
+        #     "title": "Working with preprocessing layers",
+        # },
+        # {
+        #     "path": "working_with_rnns",
+        #     "title": "Working with recurrent neural networks",
+        # },
+        # {
+        #     "path": "understanding_masking_and_padding",
+        #     "title": "Understanding masking & padding",
+        # },
+        # {
+        #     'path': 'writing_your_own_metrics',
+        #     'title': 'Writing your own Metrics',
+        # },
+        # {
+        #     'path': 'writing_your_own_losses',
+        #     'title': 'Writing your own Losses',
+        # },
+        # {
+        #     'path': 'tpu_training',
+        #     'title': 'Training Keras models on TPU',
+        # },
+        # {
+        #     'path': 'hyperparameter_optimization',
+        #     'title': 'Hyperparameter optimization',
+        # },
+        # {
+        #     'path': 'mixed_precision',
+        #     'title': 'Mixed precision training',
+        # },
+        KT_GUIDES_MASTER,
+        CV_GUIDES_MASTER,
+        NLP_GUIDES_MASTER,
+        HUB_GUIDES_MASTER,
+    ],
+}
---
+++ scripts/master.py
@@ -0,0 +1,58 @@
+from guides_master import GUIDES_MASTER
+from examples_master import EXAMPLES_MASTER
+from api_master import API_MASTER
+from keras2_api_master import KERAS2_API_MASTER
+
+MASTER = {
+    "path": "/",
+    "title": "Keras: the Python Deep Learning library",
+    "children": [
+        {
+            "path": "about",
+            "title": "About Keras",
+        },
+        {
+            "path": "getting_started/",
+            "title": "Getting started",
+            "children": [
+                {
+                    "path": "intro_to_keras_for_engineers",
+                    "title": "Introduction to Keras for engineers",
+                },
+                {
+                    "path": "benchmarks",
+                    "title": "Keras 3 benchmarks",
+                },
+                {
+                    "path": "ecosystem",
+                    "title": "The Keras ecosystem",
+                },
+                {
+                    "path": "faq",
+                    "title": "Frequently Asked Questions",
+                    "outline": False,
+                },
+            ],
+        },
+        GUIDES_MASTER,
+        API_MASTER,
+        KERAS2_API_MASTER,
+        EXAMPLES_MASTER,
+        {
+            "path": "keras_tuner/",
+            "title": "KerasTuner: Hyperparameter Tuning",
+        },
+        {
+            "path": "keras_hub/",
+            "title": "KerasHub: Pretrained Models",
+        },
+        {
+            "path": "keras_cv/",
+            "title": "KerasCV: Computer Vision Workflows",
+        },
+        {
+            "path": "keras_nlp/",
+            "title": "KerasNLP: Natural Language Workflows",
+        },
+    ],
+}
